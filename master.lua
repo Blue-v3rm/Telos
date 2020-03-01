99edbe683f669d71be6d38d9b635bbc7e9ea7135
Blue-v3rm <61633835+Blue-v3rm@users.noreply.github.com>
2020-02-29 22:57:57 -0500

    Update master.lua

--- master.lua
+++ master.lua
@@ -1,5 +1,6 @@
 local Places = {
-	[4704006941] = {"blade throwing sim",Color3.fromRGB(0,200,83),"https://raw.githubusercontent.com/Blue-v3rm/Telos/master/4704006941.lua"}
+	[4704006941] = {"blade throwing sim",Color3.fromRGB(0,200,83),"https://raw.githubusercontent.com/Blue-v3rm/Telos/master/4704006941.lua"},
+	[48994649] = {"bloodfest",Color3.fromRGB(240,0,0),"https://raw.githubusercontent.com/Blue-v3rm/Telos/master/48994649.lua"}
 }
 
 return Places
